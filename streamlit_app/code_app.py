071c43c876f1b3bc69d33bfbe51387f19d68257b
Miroslav Petreski <59763166+Miniteck@users.noreply.github.com>
2021-06-28 19:14:21 +0200

    Delete streamlit_app directory

--- streamlit_app/code_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def app():
-    st.markdown(""" ## Page: **Full Code**""")
-
-    # Set the intro text of the page
-    st.title("Individual Household Power Consumption")
-    st.markdown("____")
-    st.markdown("""Bellow is the full code of the project.""")
