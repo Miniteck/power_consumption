f7fb3c5357d21014d1d8ed6d9161418d9e76ca8a
Miroslav Petreski <59763166+Miniteck@users.noreply.github.com>
2021-06-28 18:54:23 +0200

    Add files via upload

---
+++ streamlit_app/code_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def app():
+    st.markdown(""" ## Page: **Full Code**""")
+
+    # Set the intro text of the page
+    st.title("Individual Household Power Consumption")
+    st.markdown("____")
+    st.markdown("""Bellow is the full code of the project.""")
